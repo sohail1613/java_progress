f3c5f79c1eeee14ef703575c732c0001c09c8999
sohail1613 <sohail1613@gmail.com>
2025-06-14 03:00:41 +0530

    commented

--- src/main/java/com/example/crud/entity/User.java
+++ src/main/java/com/example/crud/entity/User.java
@@ -16,6 +16,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //i should add parents in it, then make it more realistic as well
     private Long id;
     private String name;
     private String surName;
